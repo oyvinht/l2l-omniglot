970491f4f58ecc0323d011ef08d1731f759b39ee
Garibaldi Pineda Garcia <pinedagarcia1@juwels01.ib.juwels.fzj.de>
2019-09-30 16:47:29 +0200

    added install for swig

--- install_venv.sh
+++ install_venv.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
 JUBE=0
-VENV=`pwd`/venv3
-GENN=`pwd`/genn
-PYNN=`pwd`/pynn_genn
-L2L=`pwd`/L2L
+BASE=`pwd`
+VENV=$BASE/venv3
+GENN=$BASE/genn
+PYNN=$BASE/pynn_genn
+L2L=$BASE/L2L
+SWIG_TMP=$BASE/swig_tmp
+SWIG=$BASE/swig
 NVCC=`which nvcc`
 CUDA=`echo $NVCC | sed 's/\/bin\/nvcc//g'`
 
 
+echo "Installing SWIG"
+git clone https://github.com/swig/swig $SWIG_TMP
+cd $SWIG_TMP
+./autogen.sh
+./configure --prefix=$SWIG
+make
+make install
+
+echo "Installing VENV"
+cd $BASE
 python3 -m venv $VENV
 
 echo "export CUDA_PATH=$CUDA" >> $VENV/bin/activate
+echo "export PATH=$PATH:$SWIG/bin" >> $VENV/bin/activate
 
 source $VENV/bin/activate
 
@@ -30,14 +44,14 @@ cd $GENN
 
 make LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 make DYNAMIC=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
-make DYNAMIC=True MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
-make MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
+#make DYNAMIC=True MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
+#make MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 
 
 make CPU_ONLY=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 make CPU_ONLY=True DYNAMIC=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
-make CPU_ONLY=True MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
-make CPU_ONLY=True DYNAMIC=True MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
+#make CPU_ONLY=True MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
+#make CPU_ONLY=True DYNAMIC=True MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 
 sed -i 's/numpy>1\.6, < 1\.15/numpy>1\.6/g' setup.py
 python setup.py develop
@@ -49,6 +63,10 @@ cd $PYNN
 sed -i 's/numpy>1\.6, < 1\.15/numpy>1\.6/g' setup.py
 python setup.py develop
 
+cd $BASE
+tar xf JUBE-Sandra-demonstrator.tar
+cd JUBE/JUBE
+python setup.py develop
 
 git clone https://github.com/chanokin/L2L $L2L
 cd $L2L
@@ -58,4 +76,6 @@ then
 else
     python setup.py develop
 fi
+
+
 
