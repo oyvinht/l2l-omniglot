6569b14d7c0b09420e9b6a51161a9464285f3c18
Gary <chanokin@gmail.com>
2019-10-02 10:27:51 +0100

    update to install script

--- install_venv.sh
+++ install_venv.sh
@@ -10,22 +10,31 @@ SWIG_TMP=$BASE/swig_tmp
 SWIG=$BASE/swig
 NVCC=`which nvcc`
 CUDA=`echo $NVCC | sed 's/\/bin\/nvcc//g'`
+INSTALL_SWIG=0
+INSTALL_JUBE=0
 
+if [ $INSTALL_SWIG -eq 1 ]
+then
+  echo "Installing SWIG"
+  git clone https://github.com/swig/swig $SWIG_TMP
+  cd $SWIG_TMP
+  ./autogen.sh
+  ./configure --prefix=$SWIG
+  make
+  make install
+fi
 
-echo "Installing SWIG"
-git clone https://github.com/swig/swig $SWIG_TMP
-cd $SWIG_TMP
-./autogen.sh
-./configure --prefix=$SWIG
-make
-make install
-
-echo "Installing VENV"
+echo 'Installing venv'
 cd $BASE
+echo $BASE
+echo $VENV
+
 python3 -m venv $VENV
 
-echo "export CUDA_PATH=$CUDA" >> $VENV/bin/activate
-echo "export PATH=$PATH:$SWIG/bin" >> $VENV/bin/activate
+printf "\nexport CUDA_PATH=%s\n" $CUDA >> $VENV/bin/activate
+printf "\nexport PATH=\$PATH:%s/bin\n" $SWIG >> $VENV/bin/activate
+printf "\nexport PATH=\$PATH:%s/bin\n" $GENN >> $VENV/bin/activate
+ln -s $BASE/omnigloter $VENV/lib/python3.5/site-packages/
 
 source $VENV/bin/activate
 
@@ -47,7 +56,6 @@ make DYNAMIC=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 #make DYNAMIC=True MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 #make MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 
-
 make CPU_ONLY=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 make CPU_ONLY=True DYNAMIC=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 #make CPU_ONLY=True MPI_ENABLE=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
@@ -56,17 +64,20 @@ make CPU_ONLY=True DYNAMIC=True LIBRARY_DIRECTORY=$GENN/pygenn/genn_wrapper/
 sed -i 's/numpy>1\.6, < 1\.15/numpy>1\.6/g' setup.py
 python setup.py develop
 
-echo "export PATH=\$PATH:$GENN/bin" >> $VENV/bin/activate
-
 git clone https://github.com/genn-team/pynn_genn $PYNN
 cd $PYNN
 sed -i 's/numpy>1\.6, < 1\.15/numpy>1\.6/g' setup.py
 python setup.py develop
 
-cd $BASE
-tar xf JUBE-Sandra-demonstrator.tar
-cd JUBE/JUBE
-python setup.py develop
+if [ $INSTALL_JUBE -eq 1 ]
+then
+    cd $BASE
+    tar xf JUBE-Sandra-demonstrator.tar
+    cd JUBE/JUBE
+    python setup.py develop
+    cd $BASE
+    rm -fr LTL
+fi
 
 git clone https://github.com/chanokin/L2L $L2L
 cd $L2L
