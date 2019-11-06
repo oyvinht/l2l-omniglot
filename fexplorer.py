09b31100fd843395261634f8d024b2fc13eefb7a
Garibaldi Pineda Garcia <gp283@inf900374.inf.susx.ac.uk>
2019-11-06 09:46:13 +0000

    local titan

--- fexplorer.py
+++ fexplorer.py
@@ -204,7 +204,7 @@ def main():
             optimizee_bounding_func=optimizee.bounding_func)
     else:
         num_generations = 1000
-        population_size = 100
+        population_size = 50
 #         population_size = 3
         parameters = GeneticAlgorithmParameters(seed=0,
                                                 popsize=population_size,
