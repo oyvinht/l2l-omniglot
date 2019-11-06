b1e4c368742f94a04c0dce17c58f6c0c367fcd4a
Gary <chanokin@gmail.com>
2019-11-06 17:16:43 +0000

    trying to inject best of best random individual if really bad score (fit = 0) NICE abstract figures

--- fexplorer.py
+++ fexplorer.py
@@ -204,12 +204,12 @@ def main():
             optimizee_bounding_func=optimizee.bounding_func)
     else:
         num_generations = 1000
-        population_size = 50
-        population_size = 5
+        population_size = 20
+        # population_size = 10
         parameters = GeneticAlgorithmParameters(seed=0,
                                                 popsize=population_size,
                                                 CXPB=0.5, # probability of mating 2 individuals
-                                                MUTPB=0.1, # probability of individual to mutate
+                                                MUTPB=1.0, # probability of individual to mutate
                                                 NGEN=num_generations,
                                                 indpb=0.1, # probability of "gene" to mutate
                                                 tournsize=50, # number of best individuals to mate
