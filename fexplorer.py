14bfefacbd5d2daa03f044bee6ece97286f62b69
Garibaldi Pineda Garcia <pinedagarcia1@juwels03.ib.juwels.fzj.de>
2019-11-27 12:29:25 +0100

    local to julich

--- fexplorer.py
+++ fexplorer.py
@@ -45,7 +45,7 @@ def main():
                       add_time=bool(1),
                       automatic_storing=bool(1),
                       log_stdout=bool(0),  # Sends stdout to logs
-                      multiprocessing=bool(0),
+                      multiprocessing=MULTIPROCESSING,
                       )
     create_shared_logger_data(logger_names=["bin", "optimizers"],
                               log_levels=["INFO", "INFO"],
@@ -95,7 +95,7 @@ def main():
         # -n num sub-procs
         # -p gpus --gres=gpu:1 --ntasks=8 --ntasks-per-node=4 --ntasks-per-core=1
         traj.f_add_parameter_to_group("JUBE_params", "exec",
-            "srun -t 00:20:00 -c 1 --gres=gpu:1 " + \
+            "srun -t 00:30:00 -c 1 --gres=gpu:1 " + \
             " -N 1 -n 1 " + \
             " python " + os.path.join(paths.root_dir_path, "run_files/run_optimizee.py"))
     else:
