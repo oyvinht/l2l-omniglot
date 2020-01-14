a7336f96930f772c5b4d7ecc1855f8a1c75b4f91
Gary <chanokin@gmail.com>
2020-01-14 16:46:56 +0000

    merged spinnaker branch to master, needs testing

--- omnigloter/neuron_model.py
+++ omnigloter/neuron_model.py
@@ -1,3 +1,6 @@
-from python_models8.neuron.builds import IF_curr_exp_i
+from omnigloter import config
 
-# from neuron_model_genn import IF_curr_exp_i
+if config.SIM_NAME == config.SPINNAKER:
+    from python_models8.neuron.builds import IF_curr_exp_i
+elif config.SIM_NAME == config.GENN:
+    from neuron_model_genn import IF_curr_exp_i
