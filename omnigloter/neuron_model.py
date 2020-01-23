03192fd31f2bd2bc4372bf3b9bed25d6d7b0cbba
Gary <chanokin@gmail.com>
2020-01-23 13:57:55 +0000

    fix location of genn custom models

--- omnigloter/neuron_model.py
+++ omnigloter/neuron_model.py
@@ -3,4 +3,4 @@ from omnigloter import config
 if config.SIM_NAME == config.SPINNAKER:
     from python_models8.neuron.builds import IF_curr_exp_i
 elif config.SIM_NAME == config.GENN:
-    from neuron_model_genn import IF_curr_exp_i
+    from .neuron_model_genn import IF_curr_exp_i
