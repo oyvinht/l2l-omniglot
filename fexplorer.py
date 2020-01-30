4002575ff1cdcd23d8a4644d1304ba7730669b08
Gary <chanokin@gmail.com>
2020-01-30 13:08:38 +0000

    read trajectories and parse them into the starting population --- tidy up

--- fexplorer.py
+++ fexplorer.py
@@ -16,6 +16,7 @@ from l2l import dict_to_list
 from omnigloter.optimizee import OmniglotOptimizee
 from omnigloter import config
 from omnigloter.evolution_optimizer import GeneticAlgorithmOptimizer, GeneticAlgorithmParameters
+from omnigloter.utils import load_last_trajs, trajectories_to_individuals
 
 logger = logging.getLogger("bin.l2l-omniglot")
 GRADDESC, EVOSTRAT, GENALG = range(3)
