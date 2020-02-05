3a0b829a0edf4d1116c4c6ff33255d364d14bc79
garibaldi <chanokin@gmail.com>
2020-02-05 16:49:09 +0000

    removed trajectories from clear script

--- clear_experiment_output.sh
+++ clear_experiment_output.sh
@@ -2,5 +2,5 @@
 D=`pwd`/L2L-OMNIGLOT
 O=`pwd`/omnigloter
 
-rm -fr $D/jube_xml $D/ready_files $D/results $D/run-num-test $D/run_files $D/trajectories \
+rm -fr $D/jube_xml $D/ready_files $D/results $D/run-num-test $D/run_files \
        $D/work $D/optimizee.bin $O/*_CODE $D/run_results
