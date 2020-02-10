3093a51e13780e18b55f6a96a4a03a1b867106d5
Garibaldi Pineda Garcia <gp283@inf900374.inf.susx.ac.uk>
2020-02-10 13:24:43 +0000

    local to titan

--- clear_experiment_output.sh
+++ clear_experiment_output.sh
@@ -3,4 +3,4 @@ D=`pwd`/L2L-OMNIGLOT
 O=`pwd`/omnigloter
 
 rm -fr $D/jube_xml $D/ready_files $D/results $D/run-num-test $D/run_files  \
-       $D/work $D/optimizee.bin $O/*_CODE $D/run_results
+       $D/work $D/optimizee.bin $O/*_CODE 
